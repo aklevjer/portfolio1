48ee28d0269f32d4192bce56aebc56158c9427de
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-02-24 20:02:09 +0100

    Added logic to close mobile nav on nav link click

--- js/ui/index.js
+++ js/ui/index.js
@@ -1,2 +1,1 @@
-export * from "./contactForm.js";
 export * from "./mobileNav.js";
--- js/ui/mobileNav.js
+++ js/ui/mobileNav.js
@@ -9,9 +9,20 @@ function toggleMobileNav(burgerBtn, headerNav) {
   headerNav.classList.toggle("header__nav--open", !isExpanded);
 }
 
+function handleNavLink(burgerBtn, headerNav) {
+  if (window.innerWidth < 768) {
+    toggleMobileNav(burgerBtn, headerNav);
+  }
+}
+
 export function initMobileNav() {
   const burgerBtn = document.querySelector(".btn--burger");
   const headerNav = document.querySelector(".header__nav");
+  const navLinks = headerNav.querySelectorAll("a");
 
   burgerBtn.addEventListener("click", () => toggleMobileNav(burgerBtn, headerNav));
+
+  navLinks.forEach((navLink) => {
+    navLink.addEventListener("click", () => handleNavLink(burgerBtn, headerNav));
+  });
 }
